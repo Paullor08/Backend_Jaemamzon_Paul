d7963626018c29228caad2ec8a7144754022b356
Paullor08 <pongthum12246@gmail.com>
2024-08-22 19:41:36 +0700

    push model

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import products from './models/testmodel.js';
 dotenv.config();
 
 
 const app = express();
+
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -12,6 +14,20 @@ mongoose.connect(process.env.MONGO_URI, {
 })
     .then(() => console.log('MongoDB Connected'))
     .catch((err) => console.error('MongoDB connection error:', err));
+
+
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.get('/products', (req, res) => {
+    res.json(products);
+});
+
+
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
---
+++ src/models/testmodel.js
@@ -0,0 +1,25 @@
+const products = [
+    {
+        id: '1',
+        name: 'Product 1',
+        price: 100.00,
+        description: 'Description for product 1',
+        image: 'https://via.placeholder.com/150'
+    },
+    {
+        id: '2',
+        name: 'Product 2',
+        price: 200.00,
+        description: 'Description for product 2',
+        image: 'https://via.placeholder.com/150'
+    },
+    {
+        id: '3',
+        name: 'Product 3',
+        price: 300.00,
+        description: 'Description for product 3',
+        image: 'https://via.placeholder.com/150'
+    }
+];
+
+export default products;
